6ae54f2ac05580cfedd39634b4b9103ec1339c0c
Philipp Hofmann <philipp.hofmann@gmx.net>
2024-07-02 16:00:07 +0200

    Erste Strukturen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -11,42 +12,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Stein-Schere-Papier',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Stein-Schere-Papier'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +32,151 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _nothing() {
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ComputerGame(title: 'Spiel gegen den Computer',)));
+                },
+              icon: Icon(Icons.computer, size: 100,),
+              tooltip: 'Spiel gegen den Computer',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            IconButton(
+              onPressed: _nothing,
+              icon: Icon(Icons.chair, size: 100,),
+              tooltip: 'Hot-Seat Spiel gegen anderen Spieler',
+            ),
+            IconButton(
+              onPressed: _nothing,
+              icon: Icon(Icons.people, size: 100,),
+              tooltip: 'Onlinespiel gegen andere Spieler',
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+
+class ComputerGame extends StatefulWidget {
+  const ComputerGame({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<ComputerGame> createState() => _ComputerGameState();
+}
+
+class _ComputerGameState extends State<ComputerGame>{
+  String userinput = "";
+  String cpuinput = "";
+  final List<String> _input = ["scissor", "stone", "paper"];
+  var rng = Random();
+
+  _fight({required String userinput}) {
+    setState(() {
+      this.userinput = userinput;
+      cpuinput = _input[rng.nextInt(3)];
+    });
+  }
+
+  String _returnFight() {
+    String result = "";
+
+    if (userinput == 'scissor') {
+      if (cpuinput == "stone")
+        result = "Dein Kontrahent hat Stein gewählt!\nDu hast verloren!";
+      else if (cpuinput == "paper")
+        result = "Dein Kontrahent hat Papier gewählt!\nDu hast gewonnen!";
+      else
+        result = "Dein Kontrahent hat Schere gewählt!\nEs ist unentschieden!";
+    };
+
+    if (userinput == 'paper') {
+      if (cpuinput == "stone")
+        result = "Dein Kontrahent hat Stein gewählt!\nDu hast gewonnen!";
+      else if (cpuinput == "paper")
+        result = "Dein Kontrahent hat Papier gewählt!\nEs ist unentschieden!";
+      else
+        result = "Dein Kontrahent hat Schere gewählt!\nDu hast verloren!";
+    };
+
+    if (userinput == 'stone') {
+      if (cpuinput == "stone")
+        result = "Dein Kontrahent hat Stein gewählt!\nEs ist unentschieden!";
+      else if (cpuinput == "paper")
+        result = "Dein Kontrahent hat Papier gewählt!\nDu hast verloren!";
+      else
+        result = "Dein Kontrahent hat Schere gewählt!\nDu hast gewonnen!";
+    };
+
+  return result;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Wähle deine Waffe!"),
+              IconButton(
+                icon: const Icon(Icons.content_cut, size: 100),
+                tooltip: "Schere",
+                onPressed: () { setState(() {_fight(userinput: 'scissor');});},
+              ),
+              IconButton(
+                onPressed: () { setState(() {_fight(userinput: 'paper');});},
+                icon: const Icon(Icons.newspaper, size: 100),
+                tooltip: "Papier",
+              ),
+              IconButton(
+                onPressed: () { setState(() {_fight(userinput: 'stone');});},
+                icon: const Icon(Icons.monitor_weight, size: 100),
+                tooltip: "Stein",
+              ),
+              Text(_returnFight()),
+            ],
+        ),
+      ),
+    );
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
